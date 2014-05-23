c738731e773fb5da0055c2b2e8a9699b83196e86
mariaAl <maria.alvarezgut@gmail.com>
2014-05-23 10:25:12 +0200

    Cositiñas

--- js/index.js
+++ js/index.js
@@ -17,26 +17,26 @@ $(document).ready(function() {
     //PRIMER ENLACE
     $('#uno').click(function() {
         
-//       $('#rowMenu').append("<div id='enlaces1' class='col-lg-2 col-md-2 col-xs-2'></div>\
-//                                <div class='col-lg-4 col-md-4 col-xs-4 cajaEnlaces caja1 text-center'>\
-//                                     <ul>\
-//                                         <li class='enlaces'><a href='#'>Atletico de madrid</a></li>\
-//                                         <li class='enlaces'><a href='#'>Barcelona</a></li>\
-//                                     </ul>\
-//                                 </div>\
-//                                 <div class='col-lg-4 col-md-4 col-xs-4 cajaEnlaces text-center' >\
-//                                     <ul>\
-//                                            <li class='enlaces'><a href='#'>Atletico de madrid</a></li>\
-//                                            <li class='enlaces'><a href='#'>Barcelona</a></li>\
-//                                     </ul>\
-//                                 </div>\
-//                                 <div class='col-lg-2 col-md-2 col-xs-2'></div>\
-//                                 ");
-        
-        
 
         if (!$('#menu').is(":visible")) {
+            $('#rowMenu').empty();
             $('#menu').slideToggle();
+             
+            $('#rowMenu').append("<div id='enlaces1' class='col-lg-2 col-md-2 col-xs-2'></div>\
+                                <div class='col-lg-4 col-md-4 col-xs-4 cajaEnlaces caja1 text-center'>\
+                                     <ul>\
+                                         <li class='enlaces'><a href='#'>Atletico de madrid</a></li>\
+                                         <li class='enlaces'><a href='#'>Barcelona</a></li>\
+                                     </ul>\
+                                 </div>\
+                                 <div class='col-lg-4 col-md-4 col-xs-4 cajaEnlaces text-center' >\
+                                     <ul>\
+                                            <li class='enlaces'><a href='#'>Atletico de madrid</a></li>\
+                                            <li class='enlaces'><a href='#'>Barcelona</a></li>\
+                                     </ul>\
+                                 </div>\
+                                 <div class='col-lg-2 col-md-2 col-xs-2'></div>\
+                                 ");
 
         } else if($( "#rowMenu" ).children().attr('id')=='enlaces1'){
             $('#menu').slideUp();
@@ -58,35 +58,30 @@ $(document).ready(function() {
                                  <div class='col-lg-2 col-md-2 col-xs-2'></div>\
                                  ");
         }
-        
-
-
     });
 
 
     //SEGUNDO ENLACE
-    $('#dos').click(function() {
-       
-//             $('#rowMenu').append("<div id='enlaces2' class='col-lg-2 col-md-2 col-xs-2'></div>\
-//                                <div class='col-lg-4 col-md-4 col-xs-4 cajaEnlaces caja1 text-center'>\
-//                                     <ul>\
-//                                         <li class='enlaces'><a href='#'>Enlace 2.1</a></li>\
-//                                         <li class='enlaces'><a href='#'>Enlace 2.2</a></li>\
-//                                     </ul>\
-//                                 </div>\
-//                                 <div class='col-lg-4 col-md-4 col-xs-4 cajaEnlaces text-center' >\
-//                                     <ul>\
-//                                            <li class='enlaces'><a href='#'>Enlace 2.3</a></li>\
-//                                            <li class='enlaces'><a href='#'>Enlace 2.4</a></li>\
-//                                     </ul>\
-//                                 </div>\
-//                                 <div class='col-lg-2 col-md-2 col-xs-2'></div>\
-//                                 ");
-        
-        
+    $('#dos').click(function() { 
 
         if (!$('#menu').is(":visible")) {
+            $('#rowMenu').empty();
             $('#menu').slideToggle();
+            $('#rowMenu').append("<div id='enlaces2' class='col-lg-2 col-md-2 col-xs-2'></div>\
+                                <div class='col-lg-4 col-md-4 col-xs-4 cajaEnlaces caja1 text-center'>\
+                                     <ul>\
+                                         <li class='enlaces'><a href='#'>Enlace 2.1</a></li>\
+                                         <li class='enlaces'><a href='#'>Enlace 2.2</a></li>\
+                                     </ul>\
+                                 </div>\
+                                 <div class='col-lg-4 col-md-4 col-xs-4 cajaEnlaces text-center' >\
+                                     <ul>\
+                                            <li class='enlaces'><a href='#'>Enlace 2.3</a></li>\
+                                            <li class='enlaces'><a href='#'>Enlace 2.4</a></li>\
+                                     </ul>\
+                                 </div>\
+                                 <div class='col-lg-2 col-md-2 col-xs-2'></div>\
+                                 ");
 
         } else if($( "#rowMenu" ).children().attr('id')=='enlaces2'){
             $('#menu').slideUp();
@@ -114,28 +109,27 @@ $(document).ready(function() {
     //TERCER ENLACE
     
     $('#tres').click(function() {
-         //$('#rowMenu').append( "<div id='enlaces3' class='col-lg-2 col-md-2 col-xs-2'></div>\
-//                                <div class='col-lg-4 col-md-4 col-xs-4 cajaEnlaces caja1 text-center'>\
-//                                     <ul>\
-//                                         <li class='enlaces'><a href='#'>Enlace 3.1</a></li>\
-//                                         <li class='enlaces'><a href='#'>Enlace 3.2</a></li>\
-//                                     </ul>\
-//                                 </div>\
-//                                 <div class='col-lg-4 col-md-4 col-xs-4 cajaEnlaces text-center' >\
-//                                     <ul>\
-//                                            <li class='enlaces'><a href='#'>Enlace 3.3</a></li>\
-//                                            <li class='enlaces'><a href='#'>Enlace 3.4</a></li>\
-//                                     </ul>\
-//                                 </div>\
-//                                 <div class='col-lg-2 col-md-2 col-xs-2'></div>\
-//                                 ");
         
-        
-
         if (!$('#menu').is(":visible")) {
+             $('#rowMenu').empty();
             $('#menu').slideToggle();
+            $('#rowMenu').append( "<div id='enlaces3' class='col-lg-2 col-md-2 col-xs-2'></div>\
+                                <div class='col-lg-4 col-md-4 col-xs-4 cajaEnlaces caja1 text-center'>\
+                                     <ul>\
+                                         <li class='enlaces'><a href='#'>Enlace 3.1</a></li>\
+                                         <li class='enlaces'><a href='#'>Enlace 3.2</a></li>\
+                                     </ul>\
+                                 </div>\
+                                 <div class='col-lg-4 col-md-4 col-xs-4 cajaEnlaces text-center' >\
+                                     <ul>\
+                                            <li class='enlaces'><a href='#'>Enlace 3.3</a></li>\
+                                            <li class='enlaces'><a href='#'>Enlace 3.4</a></li>\
+                                     </ul>\
+                                 </div>\
+                                 <div class='col-lg-2 col-md-2 col-xs-2'></div>\
+                                 ");
 
-        } else if($( "#rowMenu" ).children().attr('id')=='enlaces2'){
+        } else if($( "#rowMenu" ).children().attr('id')=='enlaces3'){
             $('#menu').slideUp();
         }else{
             $('#rowMenu').empty();
